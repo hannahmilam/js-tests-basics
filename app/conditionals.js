50414849e8675e84b2c4572eee413aacc7614e6a
Hannah Milam <hannah.milam16@gmail.com>
2021-08-23 15:41:10 -0600

    conditionals done

--- app/conditionals.js
+++ app/conditionals.js
@@ -1,9 +1,12 @@
 //1. Given two numbers, write a function that will return  the larger number
 
 function largerNum(num1, num2) {
+    if(num1 > num2) {
+        return num1;
+        }else{
+        return num2;
+        }
     // you code here
-
-
 }
 
 // 2. Given two numbers, amount correct and amount possible of a test, return the corresponding letter grade.
@@ -15,7 +18,18 @@ function largerNum(num1, num2) {
 // output: "F"
 
 function testGrader(score, possible) {
-
+var result = (score / possible) *100;
+if (result >= 90) {
+return "A" 
+} else if (result >= 80) {
+return "B"
+} else if (result >= 70) {
+return "C"
+} else if (result >= 60) {
+    return "D"
+} else {
+    return "F"
+}
 }
 
 
@@ -31,12 +45,28 @@ function testGrader(score, possible) {
 
 function timeOfDayGreeting(hour) {
     // you code below
-
-}
+    if(hour>=5 && hour<=11){
+        return "good morning"
+    } if(hour>=12 && hour<=17){
+        return "good afternoon"
+    } if(hour>=18 && hour<=21){
+        return "good evening"
+    } if(hour>=22 && hour<=24){
+       return "good night"
+    }  if(hour>=1 && hour<=4) {
+        return "good night"
+    }
+    }
 
 //4. Write a function that will take in a number and return 'fever' if it indicates a fever (over 98.6) and additionally if the person should go to the hospital (at or above 103) 'fever go to hospital' (hint: try this with string concatenation), if it is under return 'no fever'
 function isFever(temp) {
-
+if(temp >= 103) {
+    return "fever go to hospital"
+} if(temp > 98.6){
+    return "fever"
+} else {
+    return "no fever"
+}
 }
 
 //5. Write a function that takes in a car object, if it is not moving then return true
@@ -48,7 +78,11 @@ let myCar = {
 }
 
 function isStopped(car) {
-
+if(!car.moving){
+    return true
+} else {
+    return false
+}
 }
 
 //6. Write a function that returns true if a dish is yours and is dirty, or false if one of the statements is false
@@ -59,5 +93,9 @@ let dish = {
 }
 
 function washDish(dish) {
-
+    if(dish.isDirty && dish.yourDish){
+        return true
+    } else {
+        return false
+    }
 }
